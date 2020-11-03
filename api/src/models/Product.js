8c4038280490f77b5129ab12d25c5c3c248a3ea2
Nicolas Acevedo <acevedonm@gmail.com>
2020-11-03 16:40:49 -0300

    Commit prueba en modeloProductos

--- api/src/models/Product.js
+++ api/src/models/Product.js
@@ -10,3 +10,16 @@ module.exports = (sequelize) => {
     },
   });
 };
+
+
+
+
+//NOTAS PARA NICO, BORRAR: 
+/* El Modelo de producto debe tener por lo menos estos atributos:
+
+Nombre
+Descripción
+Precio
+Stock
+Pertenecer a una o más categorías.
+Tener por lo menos una imagen. */
