dd9679331835a9e4a2f22f593763c8e11798b381
rodriip95 <rodrigo.m.penela@gmail.com>
2020-11-04 14:24:26 -0300

    newFeature (client/src/components) Se creo catalogo y contenedor de productCards

---
+++ client/src/components/productList/productList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ProductCard from '../productCard/productCard';
+
+
+function ProductList (){
+    return
+    (
+        <div>
+            <ProductCard/>
+        </div>
+    )
+}
+
+export default ProductList;
