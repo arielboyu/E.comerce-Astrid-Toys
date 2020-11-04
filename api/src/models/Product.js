b030b158e83449d45a6c7dc950a87c8901627ff2
Nicolas Acevedo <acevedonm@gmail.com>
2020-11-03 23:53:41 -0300

    Feature-S04-api/src/models/Product.js refactorice el modelo Product y revise las relaciones con nahue :D

--- api/src/models/Product.js
+++ api/src/models/Product.js
@@ -1,45 +1,32 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
+const { Category } = requiere("./Category");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
+
+
 module.exports = (sequelize) => {
   // defino el modelo
-  module.exports = (sequelize) => {
-    // defino el modelo
-    sequelize.define('product', {
-      name: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      descripcion: {
-        type: DataTypes.STRING,
-        allowNull: true
-      },
-      precio: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-      },
-      stock: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-      },
-      image :{
-        type: DataTypes.BLOB,
-        allowNull: true
-      }
-    });
-  };
+  sequelize.define("product", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    descripcion: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    precio: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.BLOB,
+      allowNull: true,
+    },
+  });
 };
 
-
-
-
-//NOTAS PARA NICO, BORRAR: 
-/* El Modelo de producto debe tener por lo menos estos atributos:
-
-Nombre
-Descripción
-Precio
-Stock
-Pertenecer a una o más categorías.
-Tener por lo menos una imagen. */
-//otro
