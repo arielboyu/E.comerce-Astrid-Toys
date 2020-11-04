faf3954ab921000dd01986610876e463a3ecd78f
Nicolas Acevedo <acevedonm@gmail.com>
2020-11-03 21:00:31 -0300

    newFeature-S04-api/src/models/Product.js implemente el modelo de Product

--- api/src/models/Product.js
+++ api/src/models/Product.js
@@ -3,12 +3,31 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('product', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  });
+  module.exports = (sequelize) => {
+    // defino el modelo
+    sequelize.define('product', {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      descripcion: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      precio: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      },
+      stock: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      },
+      image :{
+        type: DataTypes.BLOB,
+        allowNull: true
+      }
+    });
+  };
 };
 
 
