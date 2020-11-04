1403fe55d742924875b1b0e17279d2978433493c
Nicolas Acevedo <acevedonm@gmail.com>
2020-11-04 15:01:02 -0300

    S16-client/src/components/Category/Category.js NewFeature component Category && add comentary code

--- client/src/components/formularioProducto/formularioProducto.js
+++ client/src/components/formularioProducto/formularioProducto.js
@@ -1,16 +1,22 @@
-import React, {Fragment} from 'react';
+import React, { Fragment } from "react";
+import Category from "../Category/Category";
 
 const Producto = () => {
-    return (
-        <form>
-            <select>
-                <option>Cargar producto</option>
-                <option>Modificar producto</option>
-                <option>Eliminar producto</option>
-            </select>
-            <button>Realizar</button>
-        </form>
-    )
-}
+  return (
+    <form>
+      <select>
+        <option>Cargar producto</option>
+        <option>Modificar producto</option>
+        <option>Eliminar producto</option>
+      </select>
+      <button>Realizar</button>
+      <Category>
+        {/* El formulario debe poder:
+            -Agregarle una o mas categorias al producto que estamos cargando
+            -Eliminar una categoria en el caso de que nos hayamos equivocado */}
+      </Category>
+    </form>
+  );
+};
 
 export default Producto;
