ad9a241902761e3fbb720c8603a4cca9bf623992
Maxi De Filippis <maxidefilippis@gmail.com>
2020-11-04 20:48:00 -0300

    fix-S08-/client/src/components- actualización visual del APP para ubicar componentes

--- client/src/components/productList/categoryList.js
+++ client/src/components/productList/categoryList.js
@@ -4,11 +4,10 @@ import Category from '../Category/Category.js';
 export default function CategoryList({categories}){
     return (
         <div>
-            <ul>{categories.map(category=><li><Category name={category.name} description={category.description} /></li>)}</ul>
+            {/* MAP COMENTADO */}
+            {/* <ul>{categories.map(category=><li><Category name={category.name} description={category.description} /></li>)}</ul> */}
         </div>
       );
 }
-
-
 
 //en un futuro al hacer clic en category deberia cambiar la route para que haga el get por category
