dd03b1fb9b280c27bb015c1a3cf985d0b60bb106
nahuelcaputto <nahuelmartinc@gmail.com>
2020-11-05 17:52:48 -0300

    newFeature (src/components/productForm/productForm.js): formUpdate/Create/Delete components call

--- client/src/components/productForm/productForm.js
+++ client/src/components/productForm/productForm.js
@@ -1,21 +1,36 @@
-import React, { Fragment } from "react";
-import Category from "../category/category";
+import React, { useEffect, useState } from "react";
+import Category from "../Category/category";
+
 
 const Producto = () => {
+  const [optionState,setOption] = useState('create')
+  function optionHandler(option){
+    setOption(option.value);
+  };
+  useEffect(()=>{
+    // setOption(option.value);
+    console.log('hola')
+  },[])
   return (
-    <form>
-      <select>
-        <option>Cargar producto</option>
-        <option>Modificar producto</option>
-        <option>Eliminar producto</option>
-      </select>
-      <button>Realizar</button>
-      <Category>
-        {/* El formulario debe poder:
-            -Agregarle una o mas categorias al producto que estamos cargando
-            -Eliminar una categoria en el caso de que nos hayamos equivocado */}
-      </Category>
-    </form>
+      <form>
+        <select onChange={optionHandler}>
+          <option value='create'>Cargar producto</option>
+          <option value='update'>Modificar producto</option>
+          <option value='delete'>Eliminar producto</option>
+        </select>
+        <div>
+          {optionState==='create'?<InputCreate/>}
+          {optionState==='create'?<InputUpdate/>}
+          {optionState==='create'?<InputDelete/>}         
+        </div>
+        
+        <button type='submit'>Realizar</button>
+        <Category>
+          {/* El formulario debe poder:
+              -Agregarle una o mas categorias al producto que estamos cargando
+              -Eliminar una categoria en el caso de que nos hayamos equivocado */}
+        </Category>
+      </form>
   );
 };
 
