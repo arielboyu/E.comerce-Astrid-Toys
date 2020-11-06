a119d9d9f29a6f71483bd017e25c960355e575b8
Nicolas Acevedo <acevedonm@gmail.com>
2020-11-05 22:54:47 -0300

    Fix&&Refactoring (src/routes/product.js) GET a products/categoria/:nombreCat funcional

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -10,46 +10,21 @@ server.get("/", (req, res, next) => {
     .catch(next);
 });
 
-
 ///products/categoria/:nombreCa
 server.get("/categoria/:nombreCat", (req, res, next) => {
   let categoryId = req.params.nombreCat;
-  //const fetchedFoo = Foo.findOne({ include: Bar });
-
-
   Category.findAll({
-	where: {id: categoryId},
-	include: [{
-		model: Product 
-		/* through: {
-		  where: {
-				// Here, `completed` is a column present at the junction table
-			categoryid: categoryId
-		  }
-		} */
-	  }]
-  }).then((products) => {
-	res.send(products);
+    where: { id: categoryId },
+    include: [
+      {
+        model: Product,
+      },
+    ],
   })
-  .catch(next);
-
-  //primero retorno de ProductsCategory todos los id_category que coincidan con nombreCat
-  //despues hago un join entre esa tabla y los productos
-
-  //me traigo las categorias id=nombreCat
-
-   
-
-  /* 	Product.findAll({
-		include: [category]
-         where:{
-            fromId : userFrom
-        } 
+    .then((products) => {
+      res.send(products);
     })
-		.then(products => {
-			res.send(products);
-		})
-		.catch(next); */
+    .catch(next);
 });
 
 module.exports = server;
