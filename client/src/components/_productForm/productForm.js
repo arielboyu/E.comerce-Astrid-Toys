30aaa174d4df5645205c8a6c029cc32447129b31
nahuelcaputto <nahuelmartinc@gmail.com>
2020-11-06 17:26:55 -0300

    feature (client/src/components/_productForm/productForm.js): Product grid

--- client/src/components/_productForm/productForm.js
+++ client/src/components/_productForm/productForm.js
@@ -1,35 +1,70 @@
 import React, { useEffect, useState } from "react";
-import Category from "../Category/category";
-
+import Category from "../_category/category.js";
+import axios from 'axios';
+const getProduct = axios.get("http://localhost:3002/products");
 
 const Producto = () => {
-  const [optionState,setOption] = useState('create')
-  function optionHandler(option){
-    setOption(option.value);
-  };
-  // useEffect(()=>{
-  //   // setOption(option.value);
-  // },[])
+  const [product, setProduct]= useState([]);
+  
+  useEffect(()=>{
+    console.log('hola',product)
+    getProduct.then((res)=>{
+      setProduct(res.data)
+    })
+    console.log('chau',product)
+  },[product])
+  
+  const removeData = (id) => {
+
+    axios.delete(`http://localhost:3002/products/${id}`).then(res => {
+        const del = product.filter(product => id !== product.id)
+        setProduct(del)
+    })
+}
   return (
-      <form>
-        <select onChange={optionHandler}>
-          <option value='create'>Cargar producto</option>
-          <option value='update'>Modificar producto</option>
-          <option value='delete'>Eliminar producto</option>
-        </select>
         <div>
-          {optionState==='create'?<createForm/>}
-          {optionState==='create'?<updateForm/>}
-          {optionState==='create'?<deleteForm/>}         
+          <button>Insert Product</button>
+          <table className="table">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Name</th>
+                <th>Description</th>
+                <th>Price</th>
+                <th>Stock</th>
+                <th>Active</th>
+                <th> </th>
+                <th> </th>
+              </tr>
+            </thead>
+            <tbody>
+                {product.map((prod)=>
+                  <tr>
+                    <th scope="row">{prod.id}</th>
+                    <td>{prod.name}</td>
+                    <td>{prod.description}</td>
+                    <td>{prod.price}</td>
+                    <td>{prod.stock}</td>
+                    <td>{prod.active}</td>
+                    <td>
+                      <button>Update</button>
+                    </td>
+                    <td>
+                      <button onClick={() => removeData(prod.id)}>Delete</button>
+                    </td>
+                  </tr>
+                )}
+            </tbody>
+          </table>
         </div>
-        
-        <button type='submit'>Realizar</button>
-        <Category>
-          {/* El formulario debe poder:
-              -Agregarle una o mas categorias al producto que estamos cargando
-              -Eliminar una categoria en el caso de que nos hayamos equivocado */}
-        </Category>
-      </form>
+
+        // // <button type='submit'>Realizar</button>
+        // <Category>
+        //   {/* El formulario debe poder:
+        //       -Agregarle una o mas categorias al producto que estamos cargando
+        //       -Eliminar una categoria en el caso de que nos hayamos equivocado */}
+        // </Category>
+
   );
 };
 
