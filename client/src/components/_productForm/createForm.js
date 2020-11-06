2badc66590338b851824c47cf568e2438a0fd451
nahuelcaputto <nahuelmartinc@gmail.com>
2020-11-06 12:38:22 -0300

    feature(client/src/components/_productForm/createForm.js):Fields added

--- client/src/components/_productForm/createForm.js
+++ client/src/components/_productForm/createForm.js
@@ -1,16 +1,21 @@
 import { Fragment } from 'react';
 import React, Fragment from "react";
 const { Product } = require('./api/src/db.js');
-
+ //CONTINUAR   
 const CreateForm = ()=> {
     return(
         <Fragment>
             <label for='prodName'>*Product Name:</label>
-            <input tyoe='text' id='prodName' name='prodName'></input>
+            <input type='text' id='prodName' name='prodName'></input>
             <label for='prodDescr'>Product Description:</label>
-            <input tyoe='text' id='prodDescr' name='prodDescr'></input>
+            <input type='text' id='prodDescr' name='prodDescr'></input>
             <label for='prodPrice'>*Product Price:</label>
-            <input tyoe='text' id='prodPrice' name='prodPrice'></input>               
+            <input type='text' id='prodPrice' name='prodPrice'></input>
+            <label for='prodStock'>*Product Stock:</label>
+            <input type='text' id='prodStock' name='prodStock'></input>
+            {/* aca falta meter el imput de la imagen */}
+            <label for='prodAct'>Product Active:</label>
+            <input type='checkbox' checked='true' id='prodAct' name='prodAct'></input>                         
         </Fragment>
 
     )
