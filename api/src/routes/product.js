2feceb53de3daf50d569c15208af203509057d22
Nicolas Acevedo <acevedonm@gmail.com>
2020-11-05 21:34:17 -0300

    Feature (src/routes/product.js) la ruta retorna una categoria con todos sus productos en un array)

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -1,12 +1,55 @@
-const server = require('express').Router();
-const { Product } = require('../db.js');
+const server = require("express").Router();
+const { Product, Category } = require("../db.js");
 
-server.get('/', (req, res, next) => {
-	Product.findAll()
+//products
+server.get("/", (req, res, next) => {
+  Product.findAll()
+    .then((products) => {
+      res.send(products);
+    })
+    .catch(next);
+});
+
+
+///products/categoria/:nombreCa
+server.get("/categoria/:nombreCat", (req, res, next) => {
+  let categoryId = req.params.nombreCat;
+  //const fetchedFoo = Foo.findOne({ include: Bar });
+
+
+  Category.findAll({
+	where: {id: categoryId},
+	include: [{
+		model: Product 
+		/* through: {
+		  where: {
+				// Here, `completed` is a column present at the junction table
+			categoryid: categoryId
+		  }
+		} */
+	  }]
+  }).then((products) => {
+	res.send(products);
+  })
+  .catch(next);
+
+  //primero retorno de ProductsCategory todos los id_category que coincidan con nombreCat
+  //despues hago un join entre esa tabla y los productos
+
+  //me traigo las categorias id=nombreCat
+
+   
+
+  /* 	Product.findAll({
+		include: [category]
+         where:{
+            fromId : userFrom
+        } 
+    })
 		.then(products => {
 			res.send(products);
 		})
-		.catch(next);
+		.catch(next); */
 });
 
 module.exports = server;
