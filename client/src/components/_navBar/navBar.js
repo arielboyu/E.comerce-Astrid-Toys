719a16ba7880a60beda986e2b3f8c2284f66492b
Maxi De Filippis <maxidefilippis@gmail.com>
2020-11-08 14:26:09 -0300

    Fix (api/dataProducts) Actualizacion de descripcion de datos en la BDD

--- client/src/components/_navBar/navBar.js
+++ client/src/components/_navBar/navBar.js
@@ -3,9 +3,9 @@ import SearchBar from "../_searchBar/searchBar";
 import Login from "./login";
 import Cart from "./cart";
 import { Link } from "react-router-dom";
-import './navBar.css';
+import "./navBar.css";
 
-export default function NavBar({category}) {
+export default function NavBar({ category }) {
   const link = {
     listStyle: "none",
     textDecoration: "none",
@@ -14,13 +14,9 @@ export default function NavBar({category}) {
     color: "black",
   };
 
- 
-  
   return (
     <nav className="bg-warning d-flex flex-direction-column navbar navbar-dark">
-      <h1 className="col-12 text-center px-5 pt-5 pb-3">
-        Astrid Toys
-      </h1>
+      <h1 className="col-12 text-center px-5 pt-5 pb-3">Astrid Toys</h1>
       <ul className="d-flex col-sm-12 col-md-12 col-lg-5 justify-content-center justify-content-lg-start">
         <Link to="/">
           <div className="dropdown">
@@ -29,20 +25,22 @@ export default function NavBar({category}) {
         </Link>
         <div className="dropdown">
           <button className="bg-dark dropbtn bg-dark ">Categories</button>
-            <div className="dropdown-content">
-              {category.map((cat)=>
-                (<Link to={`/categories/${cat.name.toLowerCase()}`}>{cat.name}</Link>)
-              )}
+          <div className="dropdown-content">
+            {category.map((cat) => (
+              <Link to={`/categories/${cat.name.toLowerCase()}`}>
+                {cat.name}
+              </Link>
+            ))}
           </div>
         </div>
         <Link to="/products">
-        <div className="dropdown">
-          <button className="dropbtn bg-dark">Products</button>
-        </div>
+          <div className="dropdown">
+            <button className="dropbtn bg-dark">Products</button>
+          </div>
         </Link>
         <div className="dropdown">
           <button className="dropbtn bg-dark">Dashboard</button>
-            <div className="dropdown-content">
+          <div className="dropdown-content">
             <Link to="/dashboard/product/update">Products</Link>
             <Link to="/dashboard/category/list">Category</Link>
           </div>
