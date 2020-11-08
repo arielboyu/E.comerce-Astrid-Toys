a20759a98b29dacff576767f4e6b74a5df47f6e5
sifrisky <iam.astridadriana@gmail.com>
2020-11-07 21:12:31 -0300

    fix(src/components/cart) - cambios en componente carrito

--- client/src/components/Cart/cart.js
+++ client/src/components/Cart/cart.js
@@ -3,11 +3,13 @@ let carts = document.querySelectorAll();
 for(let i=0; i < carts.length; i++) {
     carts[i].addEventListener('click', ()=> {
         cartsNumbers(products[i]);
+        totalCost(products[i])
     })
 }
 
 function onLoadCartNumbers() {
-    let productNumbers = localStorage.getItem('cartNumbers')
+    let productNumbers = localStorage.getItem('cartNumbers');
+
     if(productNumbers) {
         document.querySelector('.cart').textContent= 1;
     }
@@ -25,6 +27,7 @@ function cartsNumbers(product) {
     setItems(product);
 }
 
+//usar id o tags
 function setItems(product) {
     let cartItems = localStorage.getItem('productsInCart');
     cartItems= JSON.parse(cartItems);
@@ -46,15 +49,50 @@ function setItems(product) {
 
     localStorage.setItem('productsInCart', JSON.stringify(cartItems));
 }
-    onLoadCartNumbers();
-       
-           // localStorage.setItems('cartNumbers', productsNumbers + 1);
-         
-       
+
+function totalCost(product) {
+    let cartCost = localStorage.getItem('totalCost');
+
+    if(cartCost !== null) {
+        cartCost = parseInt(cartCost);
+        localStorage.setItems('totalCost', cartCost + product.price);
+    } else {
+        localStorage.setItems('totalCost', product.price);
+    }
 }
 
+function displayCart() {
+    let cartItems= localStorage.getItems('productsInCart');
+    cartItems= JSON.parse(cartItems);
+    let productContainer = document.querySelector(".products");
 
-      
+    if(cartItems && productContainer) {
+        productContainer.innerHTML = '';
+        Object.value(cartItems).map(item => {
+            productContainer.innerHTML +=`
+            <div class= "product">
+                <ion-icon name= "close-circle"></ion-icon>
+                <img src="./images/${item.id}.jpg"/>
+                <span> ${item.name}</span>
+            </div>
+                 <div class ="price">${item.price},00</div>
+                 <div class="quantity">
+            </div>
+                <ion-icon class="decrease" name="arrow-dropleft-circle"></ion-icon>
+            </div>
+                <span>${item.inCart}</span>
+                <ion-icon class="increase" name="arrow-dropright-circle"></ion-icon>
+            </div>
+            <div class="total"> $${itemInCart * item.price},00</div>`
+            ;
+        });
 
-
-
+        productContainer.innerHTML +=`
+        <div class="basketTotalContainer">
+            <h4 class="basketTotalTitle"> basket Total</h4>
+            <h4 class="basketTotal"> $${cartCost},00</h4>`
+        ;
+    }
+}
+    onLoadCartNumbers();
+    displayCart();
