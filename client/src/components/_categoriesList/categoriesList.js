442d3bca1d0c552a155e7aa5487fbbd68d167587
arielboyu <arielboyu@gmail.com>
2020-11-07 22:28:41 -0300

    Fix(client/src/components/_categoriesList/categoriesList.js)se arreglo loop infinito

--- client/src/components/_categoriesList/categoriesList.js
+++ client/src/components/_categoriesList/categoriesList.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+
 export default function CategoriesList(){
     const getCategory = axios.get("http://localhost:3002/categories");
     const [category, setCategory]= useState([]);
@@ -7,7 +8,7 @@ export default function CategoriesList(){
         getCategory.then((res)=>{
             setCategory(res.data)
           })
-        },[category])
+        },[])
         
         const handleDelete = (c) => {
              axios.delete(`http://localhost:3002/categories/delete/${c.id}`, c)
@@ -32,11 +33,60 @@ export default function CategoriesList(){
      <td>{c.id}</td>
      <td>{c.name}</td>
      <td>{c.description}</td>
-     <button onClick={() => handleDelete(c)}>Delete</button>
+     <button
+          onClick={() => handleDelete(c)}
+          type="submit"
+          className="btn btn-primary"
+          data-toggle="modal"
+          data-target="#exampleModal"
+        >
+          Delete
+        </button>
     </tr>
     ))}
   </tbody>
-</table>
+</table> 
+        {/* <!-- Modal --> */}
+        <div
+          class="modal"
+          id="exampleModal"
+          tabindex="-1"
+          role="dialog"
+          aria-labelledby="exampleModalLabel"
+          aria-hidden="true"
+        >
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">
+                  Modal title
+                </h5>
+                <button
+                  type="button"
+                  class="close"
+                  data-dismiss="modal"
+                  aria-label="Close"
+                  onClick={() => window.location.reload()}
+                >
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">Category Deleted</div>
+              <div class="modal-footer">
+                <button
+                  type="button"
+                  class="btn btn-primary"
+                  data-dismiss="modal"
+                  onClick={() => window.location.reload()}
+                >
+                  Ok
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
+        </div>
+        
+        
     )
  }
