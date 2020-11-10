863bf71a38218adee32dfc6cf4f736ba02e1d4eb
sifrisky <iam.astridadriana@gmail.com>
2020-11-10 18:46:42 -0300

    Fix(src/components/cart) - cambios boton carrito

--- client/src/components/_cart/cart.js
+++ client/src/components/_cart/cart.js
@@ -2,90 +2,39 @@
 import React, { useState } from "react";
 import {Link} from "react-router-dom";
 
-//let carrito = [];
+let carrito = [];
 
 //cart -product cart o pedido   
     const Cart = () => {
-        const [cart, setCart]= useState([
-            {
-              id: 1,
-              name: "Harry Potter",
-              description: "Un funko de Harry Potter",
-              price: 99,
-              stock: 89,
-              image: null,
-              active: true
-            },
-            {
-              id: 2,
-              name: "Spiderman",
-              description: "Un funko de Spiderman",
-              price: 80,
-              stock: 16,
-              image: null,
-              active: true
-            },
-            {
-              id: 3,
-              name:" Hulk",
-              description:" Un funko de Hulk",
-              price: 42,
-              stock: 51,
-              image: null,
-              active: true
-            },
-            {
-              id: 4,
-              name: "Tai",
-              description: "Un funko de Tai",
-              price: 10,
-              stock: 5,
-              image: null,
-              active: true
-            },
-            {
-              id: 5,
-              name: "Bart",
-              description: "Un funko de Bar",
-              price: 54,
-              stock: 53,
-              image: null,
-              active: true
-            },
-            {
-              id: 6,
-              name: "Capitan",
-              description: "Serie",
-              price: 38,
-              stock: 76,
-              image: null,
-              active: true
-            }]);
+        const [cart, setCart]= useState([])
 
-        /*function addCart() { 
-            setCart('My Shopping Cart');
+          /*function addCart() { 
+           setCart('My Shopping Cart');
          }*/
-      if(cart) {
+
         return (
-            <table>
-                {cart.map((function) => (
-                    <tr>
-                    <th>Name</th>
-                    <th>Price</th>
-                    <th>Units</th>
-                    <th>Description</th>
-                    <th>Delete Item</th>
-                    </tr>
-                )  
-            )       
-                  <button onClick ={() => addCart() } id="boton-vaciar" class="btn btn-danger m-2">Add to Cart</button>
-                  <h1> {cart} </h1>
-          
-                  <button id="boton-vaciar" class="btn btn-danger m-2">Empty Cart</button>
-                     <h1> My Cart is empty</h1>
-              </table>  
-          )
-      }
+  
+            <div className="d-flex flex-column text-center w-75 w-m-50 mx-auto mt-5">
+                <div class="">
+                    <h1 class="display-3">My cart</h1>
+                    <p class="lead">
+                        Aun no tienes productos agregados, visita nuestro sección de productos para empezar a comprar
+                    </p>
+                    <hr class="my-2" />
+                    <p class="lead">
+                    <Link to="/products" className="text-decoration-none">
+                        <button class="btn btn-primary btn-lg my-5">
+                            IR A LA TIENDA{" "}
+                        </button>
+                    </Link>
+                    </p> 
+                      <button id="boton-agregar" class="btn btn-danger m-2">Add to Cart</button>
+                   <button id="boton-vaciar" class="btn btn-danger m-2">Empty Cart</button>      
+                </div>
+            </div>
+        )
+    }
+    
     
      export default Cart;
 
@@ -104,3 +53,27 @@ import {Link} from "react-router-dom";
     boton comprar -->  vaciar tabla pedidos (resta productos del stock)
  
     pedidos --> */
+
+         {/*} <table>
+                    <tr>
+                    <th className="m-2">Name</th>
+                    <th>Price</th>
+                    <th >Units</th>
+                    <th>Description</th>
+                    <th>DeleteItem</th>
+                    </tr>
+                {cart.map((funko) => (
+                    <tr> 
+                        <td>{funko.name}</td>
+                        <td>{funko.price}</td>
+                        <td>{funko.stock}</td>
+                        <td>{funko.description}</td>
+                        <td><button id="boton-vaciar" class="btn btn-danger m-2">Add to Cart</button>
+                        </td>
+                    </tr>
+                           </table>  
+              
+                )   
+            ) 
+              
+                */}
