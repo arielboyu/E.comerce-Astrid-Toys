2b0bafe3c8e2bb7be6b15db37e93e6b4b69ec701
Maxi De Filippis <maxidefilippis@gmail.com>
2020-11-10 00:17:42 -0300

    newFeature (/api/src/models) Creacion de modelo de Usuario

---
+++ api/src/models/User.js
@@ -0,0 +1,27 @@
+const { DataTypes } = require("sequelize");
+
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define("user", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastname: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      UNIQUE: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+};
