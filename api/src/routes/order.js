ebf5fee01e4acd0316c12ecc8712262b558e2ceb
nahuelcaputto <nahuelmartinc@gmail.com>
2020-11-11 14:52:47 -0300

    fix (api/src/routes/order.js):separated /search get of orders

--- api/src/routes/order.js
+++ api/src/routes/order.js
@@ -5,13 +5,29 @@ const { Order,Product,User, } = require('../db.js');
 // Frontend con direccion en componente orderTables.js
 // con path en http://localhost:3000/dashboard/users/list
 server.get('/', (req, res) => {
-    let whereStatement = {};
-    let state = req.query.state;
-    //S44- agrega filtro por state si es enviado por query
-    if (state){
-        whereStatement = {where:{state:state}}
-    }
-    Order.findAll(whereStatement,{
+    Order.findAll({
+        include: [
+
+                {
+                  model: Product,
+                  as: "products",
+                  attributes: [ "name", "description", "stock", "price"],
+                },
+                {
+                    model: User,
+                    as:"user",
+                    attributes: ["name" , "lastname"]
+                }
+              ],
+    })
+    .then((r) => res
+    .status(200).json(r))
+});
+
+//S44- agrega filtro por state si es enviado por query
+server.get('/search', (req, res) => {
+    let state = req.query.state
+    Order.findAll({where:{state:state},
         include: [
 
                 {
