538a40cdde1e0c2d48c14a4b086455655b8ca24c
Nicolas Acevedo <acevedonm@gmail.com>
2020-11-11 01:25:55 -0300

    retorna todas las order pending de un user

--- api/src/routes/user.js
+++ api/src/routes/user.js
@@ -33,50 +33,55 @@ server.post("/create", (req, res) => {
 });
 
 // S37 : Crear Ruta para eliminar Usuario
-// filtramos por id y utilizamos el metodo delete 
-server.delete('/delete/:id', (req,res)=>{
+// filtramos por id y utilizamos el metodo delete
+server.delete("/delete/:id", (req, res) => {
   const id = req.params.id;
   User.destroy({ where: { id } })
-  .then(user => 
-  res.status(200).send("se elimino el usuario " + user  ))
-  .catch((err) => {
-   res.status(400).send(err);
+    .then((user) => res.status(200).send("se elimino el usuario " + user))
+    .catch((err) => {
+      res.status(400).send(err);
     });
 });
 
-
 // S35 : Crear Ruta para modificar Usuario
 // Se validan los campos, se filtra por id y se actualizan los values!
-server.put('/:id',(req, res) => {
-  const {name,lastname,email,password} = req.body
-  if (!name || !lastname  || !email  || !password) {
-       res
-       .status(400)
-       .send("Debes Completar todos Los Campos ")
+server.put("/:id", (req, res) => {
+  const { name, lastname, email, password } = req.body;
+  if (!name || !lastname || !email || !password) {
+    res.status(400).send("Debes Completar todos Los Campos ");
   }
   User.findByPk(req.params.id)
-      .then((user) => {
-          user.update({
-              name: name,
-              lastname: lastname,
-              email: email,
-              password:password,
-          })
-      })
-      .then(() => {
-           res.send('El User ha sido Actualizado');
-      })
-      .catch((err) => {
-        res.status(400).send(err);
-         });
+    .then((user) => {
+      user.update({
+        name: name,
+        lastname: lastname,
+        email: email,
+        password: password,
+      });
+    })
+    .then(() => {
+      res.send("El User ha sido Actualizado");
+    })
+    .catch((err) => {
+      res.status(400).send(err);
+    });
 });
 
-server.get('/:idUser/cart', (req, res) => {
+server.get("/:idUser/cart", (req, res) => {
   const idUsuario = req.params.idUser;
-    User.findOne({ where : {idUser: idUsuario} })
-})
-.then((User) => {
-  res.send(User.getOrders({ where: { state: 'PENDING' } }))
+  User.findOne({ where: { id: idUsuario } }).then((user) => {
+    user
+      .getOrders({ where: { state: "PENDING" } })
+      .then((orders) => res.send(orders));
+  });
 });
+
+//funciones de modelos
+
+//var pepito = User.created({name: fulanito})
+
+//funciones de instancias
+
+//fulanito.getOrders()
 
 module.exports = server;
