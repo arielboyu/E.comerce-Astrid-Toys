e079687022997bd88e4e9f2197438e488f244c53
rodriip95 <rodrigo.m.penela@gmail.com>
2020-11-12 00:24:04 -0300

    newFeature (src/redux) se agrego redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
+import Store from "./redux/store/store";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
+  document.getElementById("root")
 );
-
-
---
+++ client/src/redux/reducer/reducer.js
@@ -0,0 +1,11 @@
+const initialState = {
+    
+};
+
+function rootReducer (state = initialState, action){
+
+  
+    return state
+}
+
+export default rootReducer;
