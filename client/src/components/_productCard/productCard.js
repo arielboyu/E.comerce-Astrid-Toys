b86e941bc8e6c8f4897bd7783873346a35d6e3d5
nahuelcaputto <nahuelmartinc@gmail.com>
2020-11-12 15:57:46 -0300

    newFeature (client/src/components/_productCard/productCard.js): added OUT OF STOCK logic

--- client/src/components/_productCard/productCard.js
+++ client/src/components/_productCard/productCard.js
@@ -15,13 +15,24 @@ export default function ProductCard({ product }) {
     <Link className="text-decoration-none" to={`/products/id/${product.id}`}>
       <div style={carta} className="card mx-1 my-1 p-2 shadow">
         <div style={imagen}>
-          <img style={imagen} src="https://www.cellshop.com/342978-large_default/boneca-harley-quinn-dc-super-heroes-funko-pop-301.jpg"></img>
+          <img
+            style={imagen}
+            src="https://www.cellshop.com/342978-large_default/boneca-harley-quinn-dc-super-heroes-funko-pop-301.jpg"
+          ></img>
         </div>
         <div className="card-body">
           <h3 className="m-0 text-dark">{product.name}</h3>
           <p className="mb-3 text-info">${product.price}</p>
         </div>
-        <button className="btn btn-danger mx-3 mb-1 ">ADD TO CART</button>
+        {product.stock > 0 ? (
+          <button className="btn btn-danger mx-3 mb-1 ">
+            ADD TO CART
+          </button>
+        ) : (
+          <button className="btn btn-outline-secondary mx-3 mb-1 ">
+            OUT OF STOCK
+          </button>
+        )}
       </div>
     </Link>
   );
