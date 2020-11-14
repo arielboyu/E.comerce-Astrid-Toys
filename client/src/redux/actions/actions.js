65d5e3c76809028e1ccfc09bd1caf83415164f43
rodriip95 <rodrigo.m.penela@gmail.com>
2020-11-14 02:36:43 -0300

    recuder y action addToCart

---
+++ client/src/redux/actions/actions.js
@@ -0,0 +1,36 @@
+import axios from 'axios'
+
+export function addToCart(payload){
+    return {
+        type: "ADD_CART",
+        payload
+    }
+}
+// export function getMovieDetail (payload){
+//     return {
+//         type: "GET_MOVIE_DETAIL",
+//         payload
+//     }
+// }
+
+// export function removeMovieFavorite(payload){
+//     return {
+//         type: "REMOVE_MOVIE_FAVORITE",
+//         payload
+//     }
+// }
+
+
+// export function addMovieFavorite(payload) {
+//     return { type: "ADD_MOVIE_FAVORITE", payload };
+// }
+
+// export function getMovies(titulo) {
+//     return function(dispatch) {
+//         return fetch("http://www.omdbapi.com/?apikey=20dac387&s=" + titulo)
+//         .then(response => response.json())
+//         .then(json => {
+//             dispatch({ type: "GET_MOVIES", payload: json });
+//         });
+//     };
+// }
--- client/src/redux/reducer/reducer.js
+++ client/src/redux/reducer/reducer.js
@@ -1,11 +1,34 @@
 const initialState = {
-    
+    carrito:["harry","hulk", "ironMan"],
+    user: {
+        id: 1,
+    }
 };
 
 function rootReducer (state = initialState, action){
-
+    if (action.type === "ADD_CART") {
+        return {
+          ...state,
+          carrito: state.carrito.concat(action.payload)
+        }
+    }
   
     return state
 }
 
 export default rootReducer;
+
+// function rootReducer (state = initialState, action){
+
+//     if (action.type === "GET_POKEMON"){
+//         return {
+//             ...state,
+//             pokemonDetail: action.pokeRequest.name,
+//             pokeimg: action.pokeRequest.sprites.front_default,
+//             poketype: action.pokeRequest.types[0].type.name
+//         }
+//     }
+//     return state
+// }
+
+// export default rootReducer;
