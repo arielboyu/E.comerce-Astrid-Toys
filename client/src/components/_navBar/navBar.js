92447122741b13f09785e11935fec7c9dd9c1814
Maxi De Filippis <maxidefilippis@gmail.com>
2020-11-15 19:18:28 -0300

    Fix (client/src) - Cambios al navbar

--- client/src/components/_navBar/navBar.js
+++ client/src/components/_navBar/navBar.js
@@ -19,11 +19,11 @@ export default function NavBar({ match, location }) {
   }, []);
 
   return !match.isExact ? (
-    <nav className="navbar navbar-expand-lg navbar-light bg-warning">
+    <nav className="navbar navbar-expand-lg navbar-light bg-warning pt-2 pb-0">
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-        <h1 id="title" className="col-12 col-lg-6 d-flex justify-content-center justify-content-lg-start ml-0 pl-0 ml-lg-5 pl-lg-5 bg-light"> 
+        <h1 id="title" className="col-8 col-lg-6 d-flex justify-content-start justify-content-lg-start ml-0 pl-0 ml-lg-5 pl-lg-5 pb-4 pt-4 pb-lg-0 pt-lg-0"> 
         <Link className="text-dark" to="/">Astrid Toys</Link>
         </h1>
 
@@ -38,25 +38,34 @@ export default function NavBar({ match, location }) {
             <li className="nav-item d-block d-lg-none">
               <Cart></Cart>
             </li>
-            <li className="nav-item">
+            <li className="nav-item d-block d-lg-none border-top pt-3 mt-3 border-dark">
               <Link to="/products" className="nav-link">
               <i class="fas fa-tshirt"></i> Catalogue
               </Link>
             </li>
-            <li className="nav-item active">
+
+            <li className="nav-item d-none d-lg-block">
+              <Link to="/products" className="nav-link">
+                Catalogue
+              </Link>
+            </li>
+            <li className="nav-item">
               <p className="nav-link d-none d-lg-block"> | </p>
             </li>
             
-            <li className="nav-item active">
-              <p className="nav-link mb-0 pb-2 d-block d-lg-none">Categories</p>
+            <li className="nav-item">
+              <p className="nav-link mb-0 d-block d-lg-none">
+              <i class="fas fa-list ml-1"></i> Categories</p>
             </li>
             
             {categories.map((cat) => (
             <li className="nav-item">
-              <Link className="nav-link ml-3 ml-lg-0" to={`/products/category/nav/${cat.name.toLowerCase()}`}>{cat.name}</Link>
+              <Link className="nav-link ml-4 ml-lg-0" to={`/products/category/nav/${cat.name.toLowerCase()}`}>
+                <i class="fas fa-angle-right d-inline d-lg-none"></i> {cat.name}
+              </Link>
             </li> ))} 
 
-            <li className="nav-item active">
+            <li className="nav-item text-decoration-none">
               <p className="nav-link d-none d-lg-block"> | </p>
             </li>
             
