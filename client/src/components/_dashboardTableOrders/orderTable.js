e7731457db00ebd9a36f7312cd061967e2269b04
arielboyu <arielboyu@gmail.com>
2020-11-15 17:53:44 -0300

    Fix(client/src/components/_dashboardTableOrders/orderTable.js)Correcion de sintaxis

--- client/src/components/_dashboardTableOrders/orderTable.js
+++ client/src/components/_dashboardTableOrders/orderTable.js
@@ -45,7 +45,7 @@ export default function OrderTable() {
             <td>{o.state}</td>
             <td>{o.user.username}</td>
             <td>{o.createdAt}</td>
-            <td> $ {o.products[0].orderdetails.price * o.products[0].orderdetails.quantity}</td>
+            <td> $ { o.products.length &&   o.products[0].orderdetails.price * o.products[0].orderdetails.quantity}</td>
             <td><Link to={`/dashboard/orders/detail/${o.id}`}>
              {/* para poder incluir los backticks debo colocar el path dentro de llaves */}
           <button >Detail</button>
