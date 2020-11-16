3693ab65b3d186bab63ecab85e37076d9860232d
Maxi De Filippis <maxidefilippis@gmail.com>
2020-11-16 13:38:37 -0300

    Fix (client/src/components) - Correccion de dashboard de ordenes

--- client/src/components/_dashboardTableOrders/orderTable.js
+++ client/src/components/_dashboardTableOrders/orderTable.js
@@ -25,39 +25,37 @@ export default function OrderTable() {
         .catch((err) => console.log(err));
     };
     return (
-     <div>
-         <h1>USERS ORDER LIST</h1>
-         <table class="table table-borderless">
-  <thead>
-    <tr>
-      <th>ID#</th>
-      <th>State</th>
-      <th>User Name</th>
-      <th>Discharge Date</th>
-      <th>Total</th>
-    </tr>
-  </thead>
-  <tbody>
-        {order.map((o) => (
-            <tr key={o.id}>
-            <td>{o.id}</td>
-            <td>{o.state}</td>
-            <td>{o.user.username}</td>
-            <td>{o.createdAt}</td>
-            <td> $ { o.products.length  &&   o.products[0].orderdetails.price * o.products[0].orderdetails.quantity}</td>
-            <td><Link to={`/dashboard/orders/detail/${o.id}`}>
-             {/* para poder incluir los backticks debo colocar el path dentro de llaves */}
-            <button className="btn btn-danger ml-2">Detail</button>
-            </Link></td>
-            <td><button
-            className="btn btn-danger ml-2"
-            onClick={() => {handlerCancel(o.id); window.location.reload();}}
-            >Cancel</button>
-            </td>
+      <div className="container d-flex flex-column text-center mx-auto my-5 p-5 border shadow">
+        <div>
+          <h1 className="display-3 text-center">USERS ORDER LIST</h1>
+            <table class="table table-borderless">
+            <thead>
+              <tr>
+                <th>ID#</th>
+                <th>State</th>
+                <th>User Name</th>
+                <th>Discharge Date</th>
+                <th>Total</th>
+              </tr>
+            </thead>
+            <tbody>
+              {order.map((o) => (
+              <tr key={o.id}>
+              <td>{o.id}</td>
+              <td>{o.state}</td>
+              <td>{o.user.username}</td>
+              <td>{o.createdAt}</td>
+              <td> $ { o.products.length  &&   o.products[0].orderdetails.price * o.products[0].orderdetails.quantity}</td>
+              <td><Link to={`/dashboard/orders/detail/${o.id}`}>
+              {/* para poder incluir los backticks debo colocar el path dentro de llaves */}
+                <button className="btn btn-danger ml-2">Detail</button>
+              </Link></td>
+            <td><button className="btn btn-danger ml-2" onClick={() => {handlerCancel(o.id); window.location.reload();}}>Cancel</button></td>
             </tr>    
            ))}
-        </tbody>
-     </table>
-    </div>   
+            </tbody>
+            </table>
+        </div>   
+      </div>
     );
 } 
