835f9083e01760c0dddeb1b1f3664093bdcb68c3
rodriip95 <rodrigo.m.penela@gmail.com>
2020-11-17 03:22:24 -0300

    Cart refactor

--- client/src/components/_cart/cart.js
+++ client/src/components/_cart/cart.js
@@ -1,7 +1,14 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { removeProductToCart , removeAllProductsToCart, calculeAllCart, addQuantity, subQuantity } from "../../redux/actions/actions";
+import {
+  removeProductToCart,
+  removeAllProductsToCart,
+  calculeAllCart,
+  addQuantity,
+  subQuantity,
+} from "../../redux/actions/actions";
+import Style from "./cart.css";
 
 import { Link } from "react-router-dom";
 
@@ -41,12 +48,12 @@ const Cart = () => {
   const handlerAddQuantity = (f) => {
     dispatch(addQuantity(f));
     setList(!isUpdateList);
-  }
-  
+  };
+
   const handlerSubQuantity = (f) => {
     dispatch(subQuantity(f));
     setList(!isUpdateList);
-  }
+  };
   /*const handlerCalculeAll = (f) => {
     dispatch(calculeAllCart());
     setList(!isUpdateList);
@@ -54,37 +61,78 @@ const Cart = () => {
 
   return (
     <>
-    
       <div className="container d-flex flex-column text-center my-5 p-5 border shadow">
-
-          <h1 className="display-3">My cart</h1>
-          {cart.map((f) => (
-            <div className="row">
-              <div className="col-sm-4">
-                <img src={f.image} alt={`Picture of ${f.name}`}/>
-                
+        <h1 className="display-3">My cart</h1>
+        {cart.length === 0 ? (
+          <h2>Cart empty</h2>
+        ) : (
+          <div className="row headCont">
+            <div className="col-4 col-md-8">
+              <span>Items:</span>
+            </div>
+            <div className="col-4 col-md-2">
+              <span>QTY:</span>
+            </div>
+            <div className="col-4 col-md-2">
+              <span>Price:</span>
+            </div>
+          </div>
+        )}
+        {cart.map((f) => (
+          <div className="row headCont">
+            <div className="col-4 col-md-4 imageCont">
+              <img style={{maxWidth: "100px"}} src={f.image} alt={`Picture of ${f.name}`} />
+            </div>
+            <div className="col-8 col-md-2">
+              <h3>{f.name}</h3>
+            </div>
+            <div className="col-4 col-md-2">
+              <button
+                className="btnTrash"
+                onClick={() => handlerRemove(f)}
+                type="button"
+              >
+                <i className="fas fa-trash-alt"></i>
+              </button>
+            </div>
+            <div className="col-4 col-md-2">
+              <div className="pillContainer">
+                <button
+                  className="btnMin"
+                  onClick={() => handlerSubQuantity(f)}
+                >
+                  <i className="fas fa-minus"></i>
+                </button>
+                <span className="cant">{f.cant}</span>
+                <button
+                  className="btnMax"
+                  onClick={() => handlerAddQuantity(f)}
+                >
+                  <i className="fas fa-plus"></i>
+                </button>
               </div>
             </div>
-          ))}
+            <div className="priceCeld col-4 col-md-2">
+              <p>$ {f.cant * f.price},00</p>
+            </div>
+          </div>
+        ))}
 
-
-          {/* esta el ex code abajo */}
-          <hr className="my-2" />
-          <p className="lead">
-            <Link to="/products" className="text-decoration-none">
-              <button className="btn btn-danger btn-lg my-5">
-                CONTINUE SHOPPING{" "}
-              </button>
-            </Link>
-          </p>
-
+        {/* esta el ex code abajo */}
+        <hr className="my-2" />
+        <p className="lead">
+          <Link to="/products" className="text-decoration-none">
+            <button className="btn btn-danger btn-lg my-5">
+              CONTINUE SHOPPING{" "}
+            </button>
+          </Link>
+        </p>
       </div>
     </>
   );
 };
 
 export default Cart;
-
 
 /*
 {cart.length > 0 ? (
