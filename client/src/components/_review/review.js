1014befc5da4b3e181922697768aeb7b663ff797
sifrisky <iam.astridadriana@gmail.com>
2020-11-17 23:00:57 -0300

    Fix (src/comp)

--- client/src/components/_review/review.js
+++ client/src/components/_review/review.js
@@ -1,10 +1,10 @@
 import React, {useState} from "react";
-import {faHome} from "@fortawesome/free-solid-svg-icons";
+import {faStar} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 const Review = () => {
     <div>
-        <FontAwesomeIcon icon = {faHome}/>
+        <FontAwesomeIcon icon = {faStar}/>
     </div>
 }
 
