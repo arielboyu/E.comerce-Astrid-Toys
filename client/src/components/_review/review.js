b5cc0e39941c366c5a87d781e2520c634a8db76e
sifrisky <iam.astridadriana@gmail.com>
2020-11-17 23:00:04 -0300

    NewFeature(src/comp/rev) - Creado componente Review

---
+++ client/src/components/_review/review.js
@@ -0,0 +1,11 @@
+import React, {useState} from "react";
+import {faHome} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+
+const Review = () => {
+    <div>
+        <FontAwesomeIcon icon = {faHome}/>
+    </div>
+}
+
+export default Review;
