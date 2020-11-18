4d00732892438b997ad8a74ed040bc72490e3165
Marcos-source <mc060966@gmail.com>
2020-11-18 18:46:10 -0300

    fix(api/src): add passport and their authenticate

--- api/src/routes/auth.js
+++ api/src/routes/auth.js
@@ -1,78 +1,41 @@
 const server = require("express").Router();
+const passport = require('passport');
 const express = require('express');
-var passport = require('passport');
-var session = require("express-session");
-var LocalStrategy = require('passport-local').Strategy;
+
 const { User } = require("../db.js");
 
-server.use(express.static("public"));
-server.use(session({ secret: 'secret', resave: false, saveUninitialized: false }));
-
-passport.use(new LocalStrategy(
-  function(username, password, done) {
-    User.findOne({ where: { username } })
-        .then((user) => {
-            if(!user) {
-              return done(null, false);
-            }
-            if(user.password != password) {
-              return done(null, false);
-            }
-            return done(null, user);
-          })
-        .catch(err => {
-          return done(err);
-    });
+server.post( '/signup', ( req, res, next ) => {
+	if ( req.isAuthenticated( ) ) {
+		return res.status( 400 ).send( { message: 'User is already logged in' } );
+	}
+  const { name, username, email, password } = req.body;
+  if (name && username && email && password) {
+    User.create({
+      name,
+      username,
+      email,
+      password,
+    })
+      .then((userCreated) => {
+        console.log("Usuario creado OK ", userCreated);
+        res.send(userCreated);
+      })
+      .catch((err) => {
+        res.status(400).send("Error al crear usuario ", err);
+      });
+  } else {
+    res.status(400).send("Error! campos sin completar");
   }
-));
+} );
 
-passport.serializeUser(function(user, done) {
-    done(null, user.id);
+server.post( '/login', passport.authenticate('local'),(req,res,next)=>{
+  res.send(req.user)
 });
-  
-passport.deserializeUser(function(id, done) {
-    User.findById(id)
-    .then((user) => { done(null, user) })
-    .catch(err => { return done(err) })
-});
-
-// Inicializa Passport y recupera el estado de autenticación de la sesión.
-server.use(passport.initialize());
-server.use(passport.session());
-
-// Middleware para mostrar la sesión actual en cada request
-server.use((req, res, next) => {
-    console.log(req.session);
-    console.log(req.user);
-    next();
-  });
-
-server.post('/login', 
-    passport.authenticate('local', { failureRedirect: '/' }),
-    (req, res) => { 
-        console.log(req.session);
-        console.log(req.user); 
-        res.send(req.user) } 
-)
 
 server.get('/logout', (req, res) => {
     req.logout();
     res.sendStatus(200);
 })
 
-//utilizar esta funcion como middleware para validar si hay logueo
-function isAuthenticated(req, res, next) {
-    if(req.isAuthenticated()) {
-      next();
-    } else {
-      res.redirect('/login');
-    }
-  }
-
-//   server.get('/profile',
-//   isAuthenticated,
-//   function(req, res){
-//     res.render('profile', { user: req.user });
-//   });
 
 module.exports = server;
