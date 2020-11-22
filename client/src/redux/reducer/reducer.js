ffda02d26edc55030ee3bcb4eaeb377eef65a26d
Maxi De Filippis <maxidefilippis@gmail.com>
2020-11-21 23:54:12 -0300

    Refactory (client/src/redux) - Refactory de reducer y creacion de actiones de login / logout

--- client/src/redux/reducer/reducer.js
+++ client/src/redux/reducer/reducer.js
@@ -1,51 +1,32 @@
-import Axios from "axios";
+import { 
+    ADD_CART, REMOVE_CART, REMOVE_ALL_CART, ADD_QUANTITY, SUB_QUANTITY, SET_USER, CLEAR_USER, SET_CATEGORIES 
+} from '../constants';
 
 const initialState = {
-    carrito:[],
+    carrito: [] ,
+    categories: [],
     user: {
         id: null,
         isAdmin: false,
-        name: ""
+        name: "",
     }
 };
 
 function rootReducer (state = initialState, action){
-    if (action.type === "ADD_CART") {
+    if (action.type === ADD_CART) {
         let esta = false;
         state.carrito.forEach((p)=>{
-            if(p.id === action.payload.id){
-                esta = true;
-            }
-        })
-        if(!esta){
-            return {
-              ...state,
-              carrito: state.carrito.concat(action.payload)
-            }
-        }
+        if(p.id === action.payload.id){ esta = true } } )
+        if(!esta){ return { ...state, carrito: state.carrito.concat(action.payload) } }
     }
-    if (action.type === "REMOVE_CART"){
-        return{
-            ...state,
-            carrito: state.carrito.filter((e) => e.id !== action.payload.id)
-        }
+    if (action.type === REMOVE_CART){
+        return{ ...state, carrito: state.carrito.filter((e) => e.id !== action.payload.id) }
     }
-    if(action.type === "REMOVE_ALL_CART"){
-        return{
-            ...state,
-            carrito: []
-        }
+    if(action.type === REMOVE_ALL_CART){ 
+        return{ ...state, carrito: [] }
     }
-  /*if(action.type === "CALCULE_ALL_CART") {  
-    return {
-        ...state,
 
-    }
-}*/
- 
-
-
-    if(action.type === "ADD_QUANTITY"){
+    if(action.type === ADD_QUANTITY){
         let array= state.carrito
         array.forEach((p)=>{
             if(p.id === action.payload.id){
@@ -54,13 +35,10 @@ function rootReducer (state = initialState, action){
              p.stock -= 1
             }
         })        
-        return{
-            ...state,
-            carrito: array
-        }
+        return{ ...state, carrito: array }
     }
 
-    if(action.type === "SUB_QUANTITY"){
+    if(action.type === SUB_QUANTITY){
         let array= state.carrito
         let zero=false
         array.forEach((p)=>{
@@ -68,52 +46,29 @@ function rootReducer (state = initialState, action){
                 p.price = p.price - (p.price / p.cant)
                 p.cant -= 1;
                 p.stock += 1
-              
-             if (p.cant===0){zero=true}  
+            if (p.cant===0){zero=true}  
             }
         })
-        if (zero){
-            return{
-                ...state,
-                carrito: state.carrito.filter((e) => e.id !== action.payload.id)
-            }
+        if (zero){ 
+            return{ ...state, carrito: state.carrito.filter((e) => e.id !== action.payload.id) }
         }else{
-            return{
-                ...state,
-                carrito: array
-            }
-        }
-
-    }    
-    if(action.type === "USER_LOGIN"){
-        if(action.payload){
-            return{
-                ...state,
-                user : { id : action.payload.id, isAdmin : action.payload.isAdmin, name: action.payload.name }
-            }
-        } else {
-            return {
-                ...state,
-                user: { id : null, isAdmin : false, name: ""}
-            }
+            return{ ...state, carrito: array }
         }
     }
+
+    if(action.type === SET_USER){
+        return{ ...state, user : { id : action.payload.id, isAdmin : action.payload.isAdmin, name: action.payload.name } }
+    }
+
+    if(action.type === CLEAR_USER){
+        return { ...state, user: { id : null, isAdmin : false, name: ""} }
+    }
+
+    if(action.type === SET_CATEGORIES){
+        return { ...state, categories: action.payload }
+    }
+
     return state
 }
 
 export default rootReducer;
-
-// function rootReducer (state = initialState, action){
-
-//     if (action.type === "GET_POKEMON"){
-//         return {
-//             ...state,
-//             pokemonDetail: action.pokeRequest.name,
-//             pokeimg: action.pokeRequest.sprites.front_default,
-//             poketype: action.pokeRequest.types[0].type.name
-//         }
-//     }
-//     return state
-// }
-
-// export default rootReducer;
