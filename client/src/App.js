3a981edcf12863d8a39afbef4dc0c126c9328905
Maxi De Filippis <maxidefilippis@gmail.com>
2020-11-22 16:21:55 -0300

    Fix (client/src/componentes) - Fix app & add 404 component

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,6 @@
+/* CSS */
+import './index.css'
+/* React*/
 import React from "react";
 import { BrowserRouter as Router, Route, Switch, } from "react-router-dom";
 import CategoryList from "./components/_dashboardCategoriesList/categoriesList";
@@ -8,12 +11,12 @@ import AddToCart from "./components/_addToCart/addToCart.js";
 import Login from "./components/_login/login.js";
 import Register from "./components/_login/register.js";
 import Footer from "./components/_footer/footer.js";
-import Error from "./components/error"
+import NotFound from "./components/_error/error"
 /*Componente Catalogo*/
 import Catalogue from "./components/_catalogue/catalogue";
 import Product from "./components/_product/product";
 /*Componente Navbar*/
-import Navbar from "./components/_navBar/navBar";
+import Navbar from "./components/_navBar/nav.js";
 /*Seccion Dashboard*/
 import Dashboard from "./components/_dashboard/dashboard";
 import DashboardLoadCategory from "./components/_dashboardLoadCategory/dashboardLoadCategory";
@@ -28,74 +31,67 @@ import UpdateCategories from "./components/_dashboardUpdateCategories/updateCate
 import Reduxxx from './components/pruebaRedux'
 import OrderListPending from "./components/_dashboardTableOrders/OrderTablePending"
 
-
 function App() {
   return (
     <Router>
       <Route path="/" render={({match, location}) => <Navbar match={match} location={location} />} />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/login" component={Login} />
-        <Route path="/register" component={Register}/>
-        <Route path="/esta/es/la/ruta/de/rodri">
-          <Reduxxx />
-        </Route>
-        <Route path="/products/category/nav/:cat">
-          <Catalogue />
-        </Route>
-        <Route exact path="/products/id/:index">
-          <Product />
-        </Route>
-        <Route path="/products/category/">
-          <Catalogue />
-        </Route>
-        <Route path="/products?search">
-          <Catalogue />
-        </Route>
-        <Route path="/products">
-          <Catalogue />
-        </Route>
-        <Route exact path="/dashboard" component={Dashboard} />
-        <Route path="/dashboard/category/create">
-          <DashboardLoadCategory />
-        </Route>
-        <Route path="/dashboard/product/create">
-          <DashboardLoadProduct />
-        </Route>
-        <Route path="/dashboard/product/update/:id">
-          <UpdateProduct />
-        </Route>
-        <Route path="/dashboard/product/update">
-          <DashboardUpdateProduct />
-        </Route>
-        <Route path="/dashboard/category/list">
-          <CategoryList />
-        </Route>
-        <Route path="/dashboard/category/update/:idcategory">
-          <UpdateCategories />
-        </Route>
-        <Route path="/dashboard/users/list">
-          <OrderList />
-        </Route>
-        <Route path="/dashboard/orders/detail/:idorden">
-          <OrderDetail />
-        </Route>
-        <Route path="/dashboard/orders/list/pending">
-          <OrderListPending />
-        </Route>
-        <Route path="/cart">
-          <Cart />
-        </Route>{" "}
-        <Route exact path="/myshop/:idUser">
-          <ContainerMyShopping />
-        </Route>
-        <Route path="/addToCart">
-          <AddToCart />
-        </Route>
-        <Route path="*">
-          <Error></Error>
-        </Route>
-      </Switch>
+      <Route exact path="/" component={Home} />
+          <Switch >
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/register" component={Register}/>
+            <Route path="/esta/es/la/ruta/de/rodri">
+              <Reduxxx />
+            </Route>
+            <Route path="/products/category/:cat">
+              <Catalogue />
+            </Route>
+            <Route exact path="/products/id/:index">
+              <Product />
+            </Route>
+            <Route exact path="/products?search">
+              <Catalogue />
+            </Route>
+            <Route exact path="/products">
+              <Catalogue />
+            </Route>
+            <Route exact path="/dashboard" component={Dashboard} />
+            <Route path="/dashboard/category/create">
+              <DashboardLoadCategory />
+            </Route>
+            <Route exact path="/dashboard/product/create">
+              <DashboardLoadProduct />
+            </Route>
+            <Route exact path="/dashboard/product/update/:id">
+              <UpdateProduct />
+            </Route>
+            <Route exact path="/dashboard/product/update">
+              <DashboardUpdateProduct />
+            </Route>
+            <Route exact path="/dashboard/category/list">
+              <CategoryList />
+            </Route>
+            <Route exact path="/dashboard/category/update/:idcategory">
+              <UpdateCategories />
+            </Route>
+            <Route exact path="/dashboard/users/list">
+              <OrderList />
+            </Route>
+            <Route exact path="/dashboard/orders/detail/:idorden">
+              <OrderDetail />
+            </Route>
+            <Route exact path="/dashboard/orders/list/pending">
+              <OrderListPending />
+            </Route>
+            <Route exact path="/cart">
+              <Cart />
+            </Route>
+            <Route exact path="/myshop/:idUser">
+              <ContainerMyShopping />
+            </Route>
+            <Route exact path="/addToCart">
+              <AddToCart />
+            </Route>
+          </Switch>
       <Route path="/" render={({match}) => <Footer match={match} />} />
     </Router>
   );
