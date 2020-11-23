29a47c13c5f7c6e3c4a39c6f833c32520fe402ae
Maxi De Filippis <maxidefilippis@gmail.com>
2020-11-22 22:12:49 -0300

    newFeature (client/src/shoppingHistory) - Actualizacion visual de shopping history

--- client/src/components/_myShopping/purchase.js
+++ client/src/components/_myShopping/purchase.js
@@ -29,70 +29,77 @@ const Purchase = ({ orderId }) => {
   return (
     <>
       <div className={`${style.containerShop} text-center mb-0 pb-0`}>
-        <div className="row mb-3">
-          <div className={`${style.yellow1} col-sm-12 col-md-4 px-5 py-2 d-flex flex-column`}>
-            <p>{"Order Number: " + order.id}</p>
-            <p>{"State: " + order.state}</p>
-          </div>
-          <div className={`${style.yellow2} col-sm-12 col-md-4 px-5 py-2 d-flex flex-column`}>
-            <p>{"Client: " + user.name}</p>
-            <p>{console.log(order)}</p>
-            <p>{"Client Number: " + order.userId}</p>
-          </div>
-          <div className={`${style.yellow3} col-sm-12 col-md-4 px-5 py-2 d-flex flex-column`}>
-            Date: {order.createdAt}
-          </div>
+        
+        {/* CABECERA DE LA ORDEN */}
+        <div className="d-flex flex-row justify-content-between pb-2 px-5">
+          <h2>{"ORDER N° " + order.id}</h2>
+          <h3 className={order.state === 'CANCELLED' ? 'text-danger' : 'text-success'}>{order.state}</h3>
         </div>
 
+        <div className="row mb-3">
+          <div className={`${style.yellow1} col-sm-12 col-md-4 px-5 py-2 d-flex flex-column my-auto`}>
+          <p className="my-auto">CLIENT</p>
+          <p>{user.name}</p>
+        </div>
+
+        <div className={`${style.yellow2} col-sm-12 col-md-4 px-5 py-2 d-flex flex-column my-auto`}>
+          <p className="my-auto">CLIENT NUMBER</p>
+          <p>{order.userId}</p>
+        </div>
+
+        <div className={`${style.yellow3} col-sm-12 col-md-4 px-5 py-2 d-flex flex-column`}>
+          <p className="my-auto">DATE</p>
+          <p>{order.createdAt.tostring().subString(0,16)}</p>
+        </div>
+
+        </div>
       
-          {product.map((prod) => (
-            <>
-              <div className={`${style.shop} d-flex flex-row justify-content-center align-items-center p-5 mb-3`}>
-                {/* CABECERA */}
+        {/* MAP DE PRODUCTOS DE CADA ORDEN */}
+        {product.map((prod) => (
+        <>
+          <div className={`${style.shop} d-flex flex-row justify-content-center align-items-center p-5 mb-3`}>
+          
+            <div className="d-md-flex flex-md-column col-md-2 m-3">
+              <img src={prod.image} className={style.img}></img>
+              <p className="pt-2">{prod.name}</p>
+            </div>
 
-                <div className="d-md-flex flex-md-column col-md-2 m-3">
-                  <img src={prod.image} className={style.img}></img>
-                  <p className="pt-2">{prod.name}</p>
-                </div>
+            <div className="d-md-flex flex-column col-md-2 d-none">
+              <p className={style.cabecera}>Product ID</p>
+              <p> {prod.id} </p>
+            </div>
+            
+            <div className="d-flex flex-column col-md-2 ">
+              <p className={style.cabecera}>Qty</p>
+              <p>{prod.orderdetails.quantity}</p>
+            </div>
 
-                <div className="d-md-flex flex-column col-md-2 d-none">
-                  <p className={style.cabecera}>Product ID</p>
-                  <p> {prod.id} </p>
-                </div>
-                
-                <div className="d-flex flex-column col-md-2 ">
-                  <p className={style.cabecera}>Qty</p>
-                  <p>{prod.orderdetails.quantity}</p>
-                </div>
-
-                <div className="d-flex flex-column col-md-2">
-                  <p className={style.cabecera}>U/Price </p>
-                  <p>{prod.orderdetails.price}</p>
-                </div>
-                
-                <div className="d-flex flex-column col-md-2">
-                  <p className={style.cabecera}>Sub-Total</p>
-                  <p>{prod.orderdetails.price * prod.orderdetails.quantity }</p>
-                </div>
-                
-                <div className="d-flex flex-md-column ">
-                  <Link to={`/products/id/${prod.id}`}>
-                    <button className={`${style.btnShop} btn btn-warning m-2 text-white`} >
-                      View
-                    </button>
-                  </Link>
-                  <Link to="/myshop/loadReview">
-                    <button className={`${style.btnShop} btn btn-warning m-2 text-white`} >
-                      Ratings
-                    </button>
-                  </Link>
-                </div>
-                
-              </div>
-              
-              
-              </>
-          ))}
+            <div className="d-flex flex-column col-md-2">
+              <p className={style.cabecera}>U/Price </p>
+              <p>{prod.orderdetails.price}</p>
+            </div>
+            
+            <div className="d-flex flex-column col-md-2">
+              <p className={style.cabecera}>Sub-Total</p>
+              <p>{prod.orderdetails.price * prod.orderdetails.quantity }</p>
+            </div>
+            
+            <div className="d-flex flex-md-column ">
+              <Link to={`/products/id/${prod.id}`}>
+                <button className={`${style.btnShop} btn btn-warning m-2 text-white`} >
+                  View
+                </button>
+              </Link>
+              <Link to="/myshop/loadReview">
+                <button className={`${style.btnShop} btn btn-warning m-2 text-white`} >
+                  Ratings
+                </button>
+              </Link>
+            </div>
+          
+          </div>
+        </>
+        ))}
        
       </div>
     </>
