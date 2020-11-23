562ade5487316f70061041f49f13c8ed587d36f0
Maxi De Filippis <maxidefilippis@gmail.com>
2020-11-23 01:46:43 -0300

    Feature (client/src/components) - Product review styled

--- client/src/components/_product/product.js
+++ client/src/components/_product/product.js
@@ -4,82 +4,74 @@ import axios from "axios";
 import AddToCart from "../_addToCart/addToCart";
 import { Link } from "react-router-dom";
 import Review from "../_review/review.js";
+import style from "./product.module.css"
 //Product
 
 export default function Product() {
-  const [fuko, setFuko] = useState([]);
+  const [funkos, setFunkos] = useState([]);
   const [load, setLoad] = useState(false);
 
   const { index } = useParams();
   useEffect(() => {
     axios.get(`${process.env.REACT_APP_API_URL}/products/${index}`).then((res) => {
-      setFuko(res.data);
+      setFunkos(res.data);
       setLoad(true);
     });
   }, [load]);
 
-  const card = {
-    margin: "10px",
-    textAlign: "center",
-    padding: "10px",
-    width: "700px",
-    height: "400px",
-  };
-  const imagen = {
-    margin: "20px auto",
-    maxWidth: "100%",
-    objectFit: "contain"
-  };
-  if (fuko) {
+  if (funkos) {
     return (
-      <>
-      <div
-        style={card}
-        className="container border d-flex flex-direction-row justify-content-center align-items-center my-5 mx-auto rounded shadow"
-      >
-        {fuko.map((f) => (
+      <div className={`${style.containerProduct} justify-content-center align-items-center my-5 mx-auto rounded border shadow`}>
+        <div className={`${style.card} d-flex flex-column flex-lg-row mx-auto mb-3`} >
+        {funkos.map((funko) => (
           <>
-            <div style={imagen}>
-              <img
-                style={imagen}
-                className="border-right px-4"
-                src={f.image}
-              ></img>
+            {/* FUNKO IMAGE */}
+            <div className={`${style.imageContainer} col-6 mx-auto d-flex align-items-center`}>
+              <img className={`${style.image} px-4`} src={funko.image}></img>
             </div>
-            <div className="mx-auto p-2 col-6">
-              <h1>{f.name}</h1>
-              <h4 className="text-danger mt-3">{f.description}</h4>
-              <h1 className="p-1 font-weight-bold mt-4"> ${f.price}</h1>
+            
+            {/* FUNKO DESCRIPTION */}
+            <div className="p-2 text-center text-lg-left mx-auto my-0 my-lg-auto">
+              <h1>{funko.name}</h1>
+              <h4 className="text-danger mt-0 mt-lg-3">{funko.description}</h4>
+              <h1 className="p-1 font-weight-bold mt-0 mt-lg-4"> ${funko.price}</h1>
               <p className="pt-3">
-                {f.stock > 11 ? `Stock available :)` : <></>}
-                {f.stock < 10 && f.stock > 1  ? `Only ${f.stock} units left, hurry up!` : <></>}
-                {f.stock === 1  ? `Only ${f.stock} unit left, last chance!` : <></>}
-                {f.stock === 0  ? `Not available :(` : <></> }
+                {funko.stock > 11 ? `Stock available :)` : <></>}
+                {funko.stock < 10 && funko.stock > 1  ? `Only ${funko.stock} units left, hurry up!` : <></>}
+                {funko.stock === 1  ? `Only ${funko.stock} unit left, last chance!` : <></>}
+                {funko.stock === 0  ? `Not available :(` : <></> }
               </p>
 
-              {/* AddToCart productId= {f.id} />  */}
-              {f.stock > 0 ? (
-               <AddToCart product= {f} /> 
-              ) : (
-                <button className="btn btn-outline-secondary">
-                  OUT OF STOCK
-                </button>
-              )}
+              {/* DINAMIC BUTTON ADD TO CART - OUT OF STOCK */}
+              {funko.stock > 0 ? (
+              <AddToCart product= {funko} /> ) : (
+              <button className="btn btn-outline-secondary">
+                OUT OF STOCK
+              </button> )}
+
             </div>
           </>
-        ))}
-      </div> 
-      <div className="d-flex col-12 mx-auto justify-content-center m-5">
-        <Link to="/products">
-        <button className="btn btn-outline-dark btn-lg ">
-        <i class="far fa-arrow-alt-circle-left"></i> BACK
-        </button>
-        </Link>
+          ))}
+          
+        
+        </div> 
+        <div className="d-flex flex-column flex-lg-row mx-auto p-0">
+          <div className="col-12 mx-auto">
+           <Review productId={index}/>
+          </div>
+        </div>
+        
+        <div className="d-flex col-12 mx-auto justify-content-center m-5">
+          <Link to="/products">
+          <button className={`${style.btnBack} btn `}>
+            BACK
+          </button>
+          </Link>
+        </div>
+        <div>
+        
+        </div>
       </div>
-      <div>
-      <Review productId={index}/>
-      </div>
-    </>
     );
   }
   return <p>404</p>;
