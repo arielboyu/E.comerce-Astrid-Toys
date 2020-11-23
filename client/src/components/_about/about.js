7b73b85c3f61568799eeb80738acee693a902031
Sifrisky <astrid.amg@gmail.com>
2020-11-23 19:56:17 -0300

    cambios About

--- client/src/components/_about/about.js
+++ client/src/components/_about/about.js
@@ -2,23 +2,136 @@ import React from "react";
 //import img from "../img/ari.jpeg";
 import "./about.css";
 
-const About = (props) => {
+const About = () => {
 
-    return (
-        <div className= "card text-center">
-            <div className= "overflow">
-                <img src= {props.imgsrc} alt="image 1"  className="card-img-top"/>
-            </div>
-            <div className="card-body text-dark">
-                <h4 className="card-title">About Title</h4>
-                <p className="card-text text-secondary">
-                    Describe lo que quieras aqui
-                </p>
-                <a href="#" className="btn-btn-outline-success">
-                    Read More
-                </a>
+        return(
+            <div class="container">
+                <h1 class="heading">Meet the Team</h1>
+                <div class="card-wrapper">
+                    <div class="card">
+                        <img src="images/card-bg.jpeg" alt="card background" class="card-img">
+                        <img src="images/img1.jpeg" alt="profile image" class="profile-img">
+                        <h1>Astrid Medina</h1>
+                        <p class="job-title">Designer</p>
+                        <p class="About">
+                            Describe lo que quieras aqui
+                        </p>
+                        <a href="#" class="btn">Contact</a>
+                        <ul class="social-media">
+                            <li><a href="#"><i class="fab fa-twitter-square"></i></a></li>
+                            <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                            <li><a href="#"><i class="fab fa-linkedin"></i></a></li>
+                        </ul>
+                    </div>
+                    <div class="card">
+                        <img src="images/card-bg.jpeg" alt="card background" class="card-img">
+                        <img src="images/img1.jpeg" alt="profile image" class="profile-img">
+                        <h1>Astrid Medina</h1>
+                        <p class="job-title">Designer</p>
+                        <p class="About">
+                            Describe lo que quieras aqui
+                        </p>
+                            <a href="#" class="btn">Contact</a>
+                        <ul class="social-media">
+                            <li><a href="#"><i class="fab fa-twitter-square"></i></a></li>
+                            <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                            <li><a href="#"><i class="fab fa-linkedin"></i></a></li>
+                        </ul>
+                        </div>
+                        <div class="card">
+                            <img src="images/card-bg.jpeg" alt="card background" class="card-img">
+                            <img src="images/img1.jpeg" alt="profile image" class="profile-img">
+                            <h1>Astrid Medina</h1>
+                            <p class="job-title">Designer</p>
+                            <p class="About">
+                                Describe lo que quieras aqui
+                            </p>
+                            <a href="#" class="btn">Contact</a>
+                            <ul class="social-media">
+                                <li><a href="#"><i class="fab fa-twitter-square"></i></a></li>
+                                <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                                <li><a href="#"><i class="fab fa-linkedin"></i></a></li>
+                            </ul>
+                            </div>
+                            <div class="card">
+                                <img src="images/card-bg.jpeg" alt="card background" class="card-img">
+                                <img src="images/img1.jpeg" alt="profile image" class="profile-img">
+                                <h1>Ariel Salcedo</h1>
+                                <p class="job-title">Full-Stack Developer</p>
+                                <p class="About">
+                                    Describe lo que quieras aqui
+                                </p>
+                                <a href="#" class="btn">Contact</a>
+                                <ul class="social-media">
+                                    <li><a href="#"><i class="fab fa-twitter-square"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-linkedin"></i></a></li>
+                                </ul>
+                            </div>
+                            <div class="card">
+                                <img src="images/card-bg.jpeg" alt="card background" class="card-img">
+                                <img src="images/img1.jpeg" alt="profile image" class="profile-img">
+                                <h1>Nahuel Caputto</h1>
+                                <p class="job-title">Full-Stack Developer</p>
+                                <p class="About">
+                                    Describe lo que quieras aqui
+                                </p>
+                                <a href="#" class="btn">Contact</a>
+                                <ul class="social-media">
+                                <li><a href="#"><i class="fab fa-twitter-square"></i></a></li>
+                                <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                                <li><a href="#"><i class="fab fa-linkedin"></i></a></li>
+                                </ul>
+                            </div>
+                            <div class="card">
+                                <img src="images/card-bg.jpeg" alt="card background" class="card-img">
+                                <img src="images/img1.jpeg" alt="profile image" class="profile-img">
+                                <h1>Nicolas Acevedo</h1>
+                                <p class="job-title">Full-Stack Developer</p>
+                                <p class="About">
+                                    Describe lo que quieras aqui
+                                </p>
+                                <a href="#" class="btn">Contact</a>
+                                <ul class="social-media">
+                                <li><a href="#"><i class="fab fa-twitter-square"></i></a></li>
+                                <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                                <li><a href="#"><i class="fab fa-linkedin"></i></a></li>
+                            </ul>
+                         </div>
+                        <div class="card">
+                            <img src="images/card-bg.jpeg" alt="card background" class="card-img">
+                            <img src="images/img1.jpeg" alt="profile image" class="profile-img">
+                                <h1>Maximiliano de Fillipis</h1>
+                                <p class="job-title">Full-Stack Developer</p>
+                                <p class="About">
+                                 Describe lo que quieras aqui
+                                </p>
+                                <a href="#" class="btn">Contact</a>
+                            <ul class="social-media">
+                                <li><a href="#"><i class="fab fa-twitter-square"></i></a></li>
+                                <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                                <li><a href="#"><i class="fab fa-linkedin"></i></a></li>
+                            </ul>
+                        </div>
+                        <div class="card">
+                            <img src="images/card-bg.jpeg" alt="card background" class="card-img">
+                            <img src="images/img1.jpeg" alt="profile image" class="profile-img">
+                                <h1>Rodrigo Penela</h1>
+                                <p class="job-title">Full-Stack developer</p>
+                                <p class="About">
+                                    Describe lo que quieras aqui
+                                </p>
+                                <a href="#" class="btn">Contact</a>
+                                <ul class="social-media">
+                                <li><a href="#"><i class="fab fa-twitter-square"></i></a></li>
+                                <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                                <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+                                <li><a href="#"><i class="fab fa-github"></i></a></li>
+                </ul>
             </div>
         </div>
+    </div>
+                
 
     );
 }
