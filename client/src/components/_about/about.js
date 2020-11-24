177cc75b31dd267c03f32a508b2976ff7a507fb6
Sifrisky <astrid.amg@gmail.com>
2020-11-23 23:20:31 -0300

    Agregadas las CARDS tarjetas de About

--- client/src/components/_about/about.js
+++ client/src/components/_about/about.js
@@ -1,13 +1,39 @@
-import React from "react";
-//import img from "../img/ari.jpeg";
+import React, {Component} from "react";
 import "./about.css";
 import CardAbout from './cardAbout.js';
+
+//import img from "./image.png";
+import img1 from "./image1.jpeg";
+import img2 from "./image2.jpeg";
+import img3 from "./image3.jpeg";
+import img4 from "./image4.jpeg";
+//import img5 from "./image5.jpeg";
+
 
 const About = () => {
 
         return(
-            <div> Hola
-                <CardAbout/>
+            <div className="container-fluid d-flex justify-content-center">
+                <div className="row">
+                    <div className="col-md-4">
+                         {/*<CardAbout imgsrc={img} />*/}
+                    </div>
+                        <div className="col-md-4">
+                         <CardAbout imgsrc={img1}/>
+                        </div>
+                        <div className="col-md-4">
+                         <CardAbout imgsrc={img2}/>
+                        </div>
+                        <div className="col-md-4">
+                         <CardAbout imgsrc={img3}/>
+                        </div>
+                        <div className="col-md-4">
+                         <CardAbout imgsrc={img4}/>
+                        </div>
+                        <div className="col-md-4">
+                         {/*<CardAbout imgsrc={img5}/>*/}
+                        </div>
+                </div>     
       </div>
                 
 
--- client/src/components/_about/cardAbout.js
+++ client/src/components/_about/cardAbout.js
@@ -1,21 +1,113 @@
 import React from "react";
-import img1 from "./image4.jpeg";
+import img1 from "./image1.jpeg";
+//import "./card-style.css";
 
 const CardAbout = props => {
     return(
-        <div className="card text-center">
-            <div className="overflow">
-                <img src= {img1} alt="image 4"/>
-            </div>
-            <div className="card-body text-dark">
-                <h4 className="card-title">Meet the team</h4>
-                <p className="card-text text-secondary">
-                    Escribe lo que quieras aqui
-                </p>
-                <a href="#" className="btn-btn-outline">Read More</a>
-            </div>
+        <div className="container">
+            <h1 class="heading">Meet the Team</h1>
+            <div className="card-wrapper">
+            <div class="card">
+                <img src= {props.imgsrc} alt="card background" className="card-img"/>
+                <img src= {props.imgsrc} alt="card background" className="profile-img"/>
+                   <h1>Astrid Medina</h1>
+                   <p className="job-title">Designer</p>
+                   <p class="About">
+                            Describe lo que quieras aqui
+                        </p>
+                        <a href="#" className="btn">Contact</a>
+                        <ul class="social-media">
+                            <li><i class="fab fa-twitter-square"></i></li>
+                            <li> <i class="fab fa-instagram"></i></li>
+                            <li><i class="fab fa-linkedin-in"></i></li>
+                            <li><i class="fab fa-github"></i></li>
+                        </ul>
+             </div> 
+             </div>
+             <div class="card">
+                <img src= {props.imgsrc} alt="card background" className="card-img"/>
+                <img src= {props.imgsrc} alt="card background" className="profile-img"/>
+                   <h1>Ariel Salcedo</h1>
+                   <p className="job-title">Full Stack Developer</p>
+                   <p class="About">
+                            Describe lo que quieras aqui
+                        </p>
+                        <a href="#" className="btn">Contact</a>
+                        <ul class="social-media">
+                            <li><i class="fab fa-twitter-square"></i></li>
+                            <li> <i class="fab fa-instagram"></i></li>
+                            <li><i class="fab fa-linkedin-in"></i></li>
+                            <li><i class="fab fa-github"></i></li>
+                        </ul>
+             </div>
+             <div class="card">
+                <img src= {props.imgsrc} alt="card background" className="card-img"/>
+                <img src= {props.imgsrc} alt="card background" className="profile-img"/>
+                   <h1>Nahuel Caputto</h1>
+                   <p className="job-title">Full Stack Developer</p>
+                   <p class="About">
+                            Describe lo que quieras aqui
+                        </p>
+                        <a href="#" className="btn">Contact</a>
+                        <ul class="social-media">
+                            <li><i class="fab fa-twitter-square"></i></li>
+                            <li> <i class="fab fa-instagram"></i></li>
+                            <li><i class="fab fa-linkedin-in"></i></li>
+                            <li><i class="fab fa-github"></i></li>
+                        </ul>
+             </div> 
+             <div class="card">
+                <img src= {props.imgsrc} alt="card background" className="card-img"/>
+                <img src= {props.imgsrc} alt="card background" className="profile-img"/>
+                   <h1>Maximiliano de Fillipis</h1>
+                   <p className="job-title">Full Stack Developer</p>
+                   <p class="About">
+                            Describe lo que quieras aqui
+                        </p>
+                        <a href="#" className="btn">Contact</a>
+                        <ul class="social-media">
+                            <li><i class="fab fa-twitter-square"></i></li>
+                            <li> <i class="fab fa-instagram"></i></li>
+                            <li><i class="fab fa-linkedin-in"></i></li>
+                            <li><i class="fab fa-github"></i></li>
+                        </ul>
+             </div> 
+             <div class="card">
+                <img src= {props.imgsrc} alt="card background" className="card-img"/>
+                <img src= {props.imgsrc} alt="card background" className="profile-img"/>
+                   <h1>Nicolas Acevedo</h1>
+                   <p className="job-title">Full Stack Developer</p>
+                   <p class="About">
+                            Describe lo que quieras aqui
+                        </p>
+                        <a href="#" className="btn">Contact</a>
+                        <ul class="social-media">
+                            <li><i class="fab fa-twitter-square"></i></li>
+                            <li> <i class="fab fa-instagram"></i></li>
+                            <li><i class="fab fa-linkedin-in"></i></li>
+                            <li><i class="fab fa-github"></i></li>
+                        </ul>
+             </div> 
+             <div class="card">
+                <img src= {props.imgsrc} alt="card background" className="card-img"/>
+                <img src= {props.imgsrc} alt="card background" className="profile-img"/>
+                   <h1>Rodrigo Penela</h1>
+                   <p className="job-title">Full Stack Developer</p>
+                   <p class="About">
+                            Describe lo que quieras aqui
+                        </p>
+                        <a href="#" className="btn">Contact</a>
+                        <ul class="social-media">
+                            <li><i class="fab fa-twitter-square"></i></li>
+                            <li> <i class="fab fa-instagram"></i></li>
+                            <li><i class="fab fa-linkedin-in"></i></li>
+                            <li><i class="fab fa-github"></i></li>
+                        </ul>
+             </div> 
+             
         </div>
     );
 }
 
 export default CardAbout;
+
