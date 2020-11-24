f54beba1ac9b94a3477798aea8a8904a7cd2778a
Maxi De Filippis <maxidefilippis@gmail.com>
2020-11-24 15:55:28 -0300

    Refactory (client/src/App) - Refactory de name de carpetas

---
+++ client/src/components/Catalogue/AddToCart/addToCart.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from "react";
+import style from "../ProductCard/productCard.module.css";
+
+import {useDispatch} from 'react-redux'
+import {addToCart} from '../../../redux/actions/actions'
+
+
+const AddToCart = ({ product }) => {
+  const [productoParaAgregar, setProducto] = useState()
+  const dispatch = useDispatch()
+
+  useEffect(()=>{
+    setProducto({...product, cant:1})
+  },[])
+ 
+  return (
+        <button 
+          id="boton-agregar"
+          className={`btn btn-danger mb-1 ${style.cart}`}
+          type="button"
+          onClick={()=>dispatch(addToCart(productoParaAgregar))}
+        >
+          <i class="fas fa-cart-plus"></i> ADD TO CART
+        </button>)
+      
+};
+
+
+export default AddToCart;
