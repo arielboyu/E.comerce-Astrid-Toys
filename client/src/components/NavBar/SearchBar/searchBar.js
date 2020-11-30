8e1080f292e0cdcc6ad235a9f0c4d6eb7d1483d4
Nicolas Acevedo <acevedonm@gmail.com>
2020-11-30 15:47:22 -0300

    last changed fix

--- client/src/components/NavBar/SearchBar/searchBar.js
+++ client/src/components/NavBar/SearchBar/searchBar.js
@@ -1,20 +1,25 @@
 import React, { useState } from 'react';
-import { Redirect } from 'react-router-dom'
+import { Redirect, useHistory } from 'react-router-dom'
 
 
 export default function SearchBar({ onSearch, onClear }) {
   const [search, setSearch] = useState();
   const [flag, setFlag] = useState(false);
+  const localy = useHistory()
 
   const handlerInput = (e) => {
     setSearch(e.target.value);
   };
-
+const handlerClear = () => {
+  onClear()
+  localy.push("/products")
+}
   const handlerSubmit = (e) => {
     e.preventDefault();
     setFlag(true);
     onSearch(search);
     setSearch("");
+    localy.push(`/products/search?data=${search}`)
   };
 
   return (
@@ -29,8 +34,8 @@ export default function SearchBar({ onSearch, onClear }) {
           ></input>
         </div>
         <button className="btn btn-dark ml-2 mr-1" type="submit"> Submit </button> 
-              {flag ? <Redirect to={`/products/search?data=${search}`} /> : <></>}
-        <button className="btn btn-dark my-sm-0" type="button" onClick={onClear} > Clear </button>
+              {/* flag ? <Redirect to={`/products/search?data=${search}`} /> : <></> */}
+        <button className="btn btn-dark my-sm-0" type="button" onClick={handlerClear} > Clear </button>
         <div>
 
         </div>
